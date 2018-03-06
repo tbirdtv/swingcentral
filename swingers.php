31c2126b32ea11151d6c4d96039451a91d69bd0b
tbirdtv <37005325+tbirdtv@users.noreply.github.com>
2018-03-05 20:03:56 -0500

    Create swingers.php

---
+++ swingers.php
@@ -0,0 +1,4 @@
+#EXTM3U
+
+#EXTINF:-1 tvg-logo="https://thumb-v-ec.xhcdn.com/t/377/7_7463377.jpg", Test
+plugin://plugin.video.monkeyspank/?url=http%3A%2F%2Fxhamster.com%2Fvideos%2Fstp4-daughter-comes-home-to-five-old-dicks-7463377&amp;mode=4&amp;name=STP4+Daughter+Comes+Home+To+Five+Old+Dicks
